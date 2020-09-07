6a09d55145585bfa8399eaa9dee96eb79bfdb6eb
porrametict <60021414@up.ac.th>
2020-09-07 11:09:24 +0700

    modify Addtendance workflow

--- src/components/classroom_modules/attendance/Attendance.vue
+++ src/components/classroom_modules/attendance/Attendance.vue
@@ -9,11 +9,12 @@
 
     <v-card-text v-if="state.component">
 
-
       <div v-if="is_host" class="text-center">
-        student(s) : {{ state.data.students.length }} / {{ state.data.all_students_count }}
-        <div v-for="student in state.data.students">
-           <p>{{ student.first_name }} {{ student.last_name }}</p>
+        student(s) : {{ state.data.present_student_count }} / {{ state.data.all_students.length }}
+        <div v-for="student in state.data.all_students">
+          <p class="title" :class="{'green--text': student['present']  ,'red--text' : !student['present'] }">
+            {{ student.user_data.first_name }}
+            {{ student.user_data.last_name }}</p>
         </div>
         <br/>
         <v-btn rounded color="blue" class="white--text" @click="end_attendance">
@@ -22,8 +23,8 @@
       </div>
 
 
-      <div v-else-if="is_viewer" class="text-center">
-        student(s) : {{ state.data.students.length }} / {{ state.data.all_students_count }}
+      <div v-else-if="is_viewer" class="text-center title">
+        student(s) : {{ state.data.present_student_count }} / {{ state.data.all_students.length }}
       </div>
 
 
@@ -40,6 +41,7 @@
 
 <script>
 import {mapState} from 'vuex'
+import moment from "moment";
 
 export default {
   name: "Attendance",
@@ -92,8 +94,8 @@ export default {
         state: 'start',
         component: null,
         data: {
-          all_students_count: 0,
-          students: []
+          room: null,
+          all_students: [],
         }
       }
     }
@@ -106,13 +108,13 @@ export default {
           `${window.baseWsURL}/attendance/${self.room.room_code}/`
       )
       this.module_socket.onopen = function () {
-        // self.get_current_state()
+        self.get_current_state()
       }
       this.module_socket.onclose = function (e) {
         console.error('Attendant socket closed unexpectedly', e);
       }
       let commands = {
-        // 'on_get_current_state': self.on_get_current_state,
+        'on_get_current_state': self.on_get_current_state,
         'on_attendance': self.on_attendance,
       }
       this.module_socket.onmessage = function (e) {
@@ -124,6 +126,43 @@ export default {
     },
     socket_send(data) {
       this.module_socket.send(JSON.stringify(data));
+    },
+
+    get_current_state() {
+      let content = {
+        "command": "get_current_state",
+        "data": {
+          "user": this.user,
+        }
+      }
+      this.socket_send(content);
+    },
+    async on_get_current_state(e) {
+      this.state = e['data']['state']
+      this.state['component'] = this.get_component_by_state(this.state)
+      this.check_attended()
+    },
+    check_attended() {
+      this.state.data.all_students.forEach(e => {
+        if (e.student === this.user.pk) {
+          if (e.present) {
+            this.end_attendance()
+          }
+        }
+      })
+
+    },
+    get_component_by_state(state, component_index = 0) {
+      let state_name = state.state
+      let component = null
+      if (this.is_host) {
+        component = state['component_set']['host'][state_name][component_index]
+      } else if (this.is_viewer) {
+        component = state['component_set']['viewer'][state_name][component_index]
+      } else {
+        component = state['component_set']['student'][state_name][component_index]
+      }
+      return component
     },
     go_attendance() {
       this.socket_send(
@@ -137,10 +176,21 @@ export default {
       this.end_attendance()
     },
     on_attendance(e) {
-      this.state.data= e.data.state.data
-
+      this.state.data = e.data.state.data
     },
-    end_attendance (){
+    end_attendance() {
+      if (this.is_host) {
+        moment.locale('th')
+        this.state.data.name = `เช็คชื่อ ${moment().format('lll')}`
+        this.socket_send(
+            {
+              "command": "save_attendance",
+              "data": {
+                "state": this.state,
+              }
+            }
+        )
+      }
       this.$emit(
           'ended', {event: 'attendance'}
       )
