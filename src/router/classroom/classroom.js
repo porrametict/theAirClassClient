b96bface33cc332f1cc74d170613f01aa0ad8cbc
Suwantha <suwanthagg@gmail.com>
2020-09-11 12:07:23 +0700

    conflices

--- src/router/classroom/classroom.js
+++ src/router/classroom/classroom.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
-import score from "../classroom_modules/score";
-import member from "../classroom_modules/member";
-import assignments from "../classroom_modules/assignments";
-import questions from "../classroom_modules/questions";
-import steam from "../steam/steam";
-
-=======
 import classroom_module_index from "../classroom_modules/classroom_module_index";
 import module_index from '../classroom_modules/index'
->>>>>>> bce56eb2712e1c8c18fee3aba672d7074c017a4a
 export default {
     path: '/classroom',
     component: () => import('../../views/classroom/Template'),
@@ -44,14 +35,6 @@ export default {
                 classroom_module_index
             ]
         },
-<<<<<<< HEAD
-        {
-            path: "steam",
-            name: "SteamsClassroom",
-            component: () => import('../../views/steams/Index')
-        },
-=======
         module_index
->>>>>>> bce56eb2712e1c8c18fee3aba672d7074c017a4a
     ]
 }
